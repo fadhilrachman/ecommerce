bd7dd6cfe51d72def63efe457af056b3481d37bb
fadhilrachman <99377200+fadhilrachman@users.noreply.github.com>
2023-03-17 10:20:45 +0700

    create boilerplate

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex border justify-between px-7  items-center ">
+      <div>
+        <img
+          src="https://themewagon.github.io/ashion/img/logo.png"
+          alt=""
+          srcset=""
+        />
+      </div>
+      <div className="flex items-center">
+        <div className="mr-4 relative py-6 px-6 ">
+          <i class="bi bi-cart text-xl"></i>
+          <div className="bg-black absolute top-3 right-3    text-white font-bold  rounded-full flex justify-center items-center text-xs w-5 h-5">
+            <small>3</small>
+          </div>
+        </div>
+        <a href="" className="text-1xl">
+          login / regsiter
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
